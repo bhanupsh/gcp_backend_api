73ef9fb58bc4bbe7db2d52efe83f3b5de470f0c8
bhanupsh <chbprakash.js@gmail.com>
2026-03-14 00:13:41 +0530

    intial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+
+
+import express from "express";
+import cors from "cors";
+import { configDotenv } from "dotenv";
+configDotenv();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send(`Hello World!., Environment:  ${process.env.ENV} `);
+});
+
+export default app;
