afd22e78696edd745fc5f60ccb5d390779b06ad2
bhanupsh <chbprakash.js@gmail.com>
2026-03-14 16:54:56 +0530

    Db connection and show comments

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
-
-
 import express from "express";
 import cors from "cors";
-import { configDotenv } from "dotenv";
-configDotenv();
+import connectDB from "./config/db.js";
+import commentsModel from "./models/comments.model.js";
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+await connectDB();
 
+// Middleware
+app.use(cors());
+app.use(express.json());   
+
+// default route
 app.get("/", (req, res) => {
-    res.send(`Hello World!., -- Version 2 -- Environment:  ${process.env.ENV} `);
+  res.send("Welcome to the Task On GCP.");
+});
+
+app.get("/api/comments", async (req, res) => {
+  try {
+    const comments = await commentsModel.find().limit(20).sort({ createdAt: -1 });
+    res.status(200).json(comments);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 export default app;
+
+
